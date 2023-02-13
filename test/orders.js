069e80b512527f00affaee5ed491f2790909e051
Qadis Chaudhry <qadis.chaudhry@gmail.com>
2023-02-13 17:23:46 -0500

    minor changes

---
+++ test/orders.js
@@ -0,0 +1,17 @@
+const admin = require("./firebase").admin;
+
+var db = admin.database();
+var ref = db.ref("/Orders/");
+
+var oid = "192457395872";
+ref.child(`${oid}`).set({
+    title: "Tylenol",
+    oid: oid,
+    price: 10.00,
+    uid: "akdsjfljdas",
+    status: "verified"
+})
+
+ref.child(`${oid}`).on('value', (snapshot) => {
+    console.log(snapshot.val());
+})
